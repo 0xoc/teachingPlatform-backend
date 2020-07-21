3613613070954c8707f17dc69abed69c45ff2fa9
Ali Parvizi <snparvizi75@gmail.com>
2020-07-21 11:50:22 +0430

    bug fix

--- core/utils.py
+++ core/utils.py
@@ -5,7 +5,7 @@ def get_object(*args, **kwargs):
 
     from django.http import Http404
     try:
-        get_object_or_404(*args, **kwargs)
+        return get_object_or_404(*args, **kwargs)
     except Http404 as http404:
         from rest_framework.exceptions import APIException
         raise APIException(detail=str(http404), code=404)
