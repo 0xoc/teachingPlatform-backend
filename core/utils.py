cb091d999428bc678b3b34fd3445ca98de4d0959
Ali Parvizi <snparvizi75@gmail.com>
2020-07-21 12:27:26 +0430

    bug fix

--- core/utils.py
+++ core/utils.py
@@ -1,11 +1,15 @@
+from rest_framework.exceptions import APIException
+
+
+class NotFound(APIException):
+    status_code = 404
+
 
 def get_object(*args, **kwargs):
-
     from django.shortcuts import get_object_or_404
 
     from django.http import Http404
     try:
         return get_object_or_404(*args, **kwargs)
     except Http404 as http404:
-        from rest_framework.exceptions import APIException
-        raise APIException(detail=str(http404), code=404)
+        raise NotFound(detail=str(http404), code=404)
